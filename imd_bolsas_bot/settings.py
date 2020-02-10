56da8151d7bb20068467fceaa24879a14b541308
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-10 13:32:30 -0300

    Registrar pipeline para filtrar notícias sem as tags desejadas

--- imd_bolsas_bot/settings.py
+++ imd_bolsas_bot/settings.py
@@ -8,7 +8,8 @@ NEWSPIDER_MODULE = 'imd_bolsas_bot.spiders'
 USER_AGENT = 'imd_bolsas_bot (+https://t.me/imdbolsasbot)'
 
 ITEM_PIPELINES = {
-    'imd_bolsas_bot.exporters.PerIDJSONExportPipeline': 1000
+    'imd_bolsas_bot.pipelines.WantedTagsFilterPipeline': 0,
+    'imd_bolsas_bot.exporters.PerIDJSONExportPipeline': 1
 }
 
 # Enable and configure HTTP caching (disabled by default)
