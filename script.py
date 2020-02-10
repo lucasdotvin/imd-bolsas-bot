3253302922224bfd55431896f67d5c687ca19ec3
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-10 13:36:22 -0300

    Limpar arquivo principal e usar OO para execução do bot

--- script.py
+++ script.py
@@ -1,46 +1,17 @@
 import os
 import configs
 
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from lib.imdnewsbot import IMDNewsBot
 
 
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Hello, world!"
+def main():
+    bot = IMDNewsBot(
+        channel_id=os.getenv('TELEGRAM_CHANNEL_ID'),
+        telegram_token=os.getenv('TELEGRAM_BOT_TOKEN')
     )
 
+    bot.run()
 
-def echo(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=update.message.text
-    )
-
-
-def register(update, context):
-    message = ' '.join(context.args)
-
-    context.bot.send_message(
-        chat_id=os.getenv('TELEGRAM_CHANNEL_ID'),
-        text=message
-    )
 
 if __name__ == '__main__':
-    updater = Updater(
-        token=os.getenv('TELEGRAM_BOT_TOKEN'),
-        use_context=True
-    )
-
-    dispatcher = updater.dispatcher
-    handlers = (
-        CommandHandler('start', start),
-        CommandHandler('register', register),
-        MessageHandler(Filters.text, echo)
-    )
-
-    for handler in handlers:
-        dispatcher.add_handler(handler)
-
-    updater.start_polling()
-    updater.idle()
+    main()
