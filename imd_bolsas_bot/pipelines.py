ce224197cc45bd04cf4cfcf4bef89bc0b48ebb86
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-10 23:15:08 -0300

    Adicionar pipeline para converter a data de publicação em um objeto datetime

--- imd_bolsas_bot/pipelines.py
+++ imd_bolsas_bot/pipelines.py
@@ -1,6 +1,37 @@
 import os
+from datetime import datetime
 
 from scrapy.exceptions import DropItem
+
+
+class SanitizeDatePipeline(object):
+    MONTH_NAME_TRANSLATION_TABLE = {
+        'fev': 'feb',
+        'abr': 'apr',
+        'mai': 'may',
+        'ago': 'aug',
+        'set': 'sep',
+        'out': 'oct',
+        'dez': 'dec'
+    }
+
+
+    def _translate_month_name(self, item_date):
+        for pt_month_name in self.MONTH_NAME_TRANSLATION_TABLE:
+            en_month_name = self.MONTH_NAME_TRANSLATION_TABLE.get(
+                pt_month_name
+            )
+
+            item_date = item_date.replace(pt_month_name, en_month_name)
+
+        return item_date
+
+
+    def process_item(self, item, spider):
+        item['date'] = self._translate_month_name(item['date'])
+        item['date'] = datetime.strptime(item['date'], r'%d %b %Y')
+        return item
+
 
 class WantedTagsFilterPipeline(object):
     def __init__(self):
